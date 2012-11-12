cd966195bc86a747990e629a062f8b0a75edae68
C3dr1c <cedric.paillat@gmail.com>
2012-11-12 21:34:46 +0100

    Refacto. des indices 8 et 9.

--- Enigme_Einstein.pl
+++ Enigme_Einstein.pl
@@ -1,8 +1,7 @@
-﻿% Les faits :
+% Les faits :
 
-% Chaque maison à 6 paramètres qui seront dans l'ordre suivant :
+% Chaque maison à 5 paramètres qui seront dans l'ordre suivant :
 
-% - Numéro
 % - Couleur
 % - Nationalité
 % - Animal domestique
@@ -10,12 +9,12 @@
 % - Cigare
 
 maisons([
-    maison(1,_,_,_,_,_) ,
-    maison(2,_,_,_,_,_) ,
-    maison(3,_,_,_,_,_) ,
-    maison(4,_,_,_,_,_) ,
-    maison(5,_,_,_,_,_)
-	  ]).
+    maison(_,_,_,_,_) ,
+    maison(_,_,_,_,_) ,
+    maison(_,_,_,_,_) ,
+    maison(_,_,_,_,_) ,
+    maison(_,_,_,_,_)
+]).
 
 % Les règles :
 
@@ -38,52 +37,52 @@ maisons(MAISONS),
 % Les indices :
 
 % 1 - L'anglais vit dans une maison rouge
-appartient_à(maison(_,rouge,anglais,_,_,_),MAISONS),
+appartient_à(maison(rouge,anglais,_,_,_),MAISONS),
 
 % 2 - Le Suédois a des chiens comme animaux domestiques.
-appartient_à(maison(_,_,suédois,chien,_,_),MAISONS),
+appartient_à(maison(_,suédois,chien,_,_),MAISONS),
 
 % 3 - Le Danois boit du thé.
-appartient_à(maison(_,_,danois,_,thé,_),MAISONS),
+appartient_à(maison(_,danois,_,thé,_),MAISONS),
 
 % 4 - La maison verte est à gauche de la maison blanche.
-est_à_gauche_de(maison(_,verte,_,_,_,_),maison(_,blanche,_,_,_,_),MAISONS),
+est_à_gauche_de(maison(verte,_,_,_,_),maison(blanche,_,_,_,_),MAISONS),
 
 % 5 - Le propriétaire de la maison verte boit du café.
-appartient_à(maison(_,verte,_,_,café,_),MAISONS),
+appartient_à(maison(verte,_,_,café,_),MAISONS),
 
 % 6 - La personne qui fume des Pall Mall a des oiseaux.
-appartient_à(maison(_,_,_,oiseaux,_,pall_mall),MAISONS),
+appartient_à(maison(_,_,oiseaux,_,pall_mall),MAISONS),
 
 % 7 - Le propriétaire de la maison jaune fume des Dunhill.
-appartient_à(maison(_,jaune,_,_,_,dunhill),MAISONS),
+appartient_à(maison(jaune,_,_,_,dunhill),MAISONS),
 
 % 8 - La personne qui vit dans la maison du centre boit du lait.
-appartient_à(maison(3,_,_,_,lait,_),MAISONS),
+MAISONS = [_,_,maison(_,_,_,lait,_),_,_],
 
 % 9 - Le Norvégien habite la première maison.
-appartient_à(maison(1,_,norvégien,_,_,_),MAISONS),
+MAISONS = [maison(_,norvégien,_,_,_)|_],
 
 % 10 - L'homme qui fume les Blend vit à côté de celui qui a des chats.
-est_à_côté_de(maison(_,_,_,_,_,blend),maison(_,_,_,chats,_,_),MAISONS),
+est_à_côté_de(maison(_,_,_,_,blend),maison(_,_,chats,_,_),MAISONS),
 
 % 11 - L'homme qui a un cheval est le voisin de celui qui fume des Dunhill.
-est_à_côté_de(maison(_,_,_,cheval,_,_),maison(_,_,_,_,_,dunhill),MAISONS),
+est_à_côté_de(maison(_,_,cheval,_,_),maison(_,_,_,_,dunhill),MAISONS),
 
 % 12 - Le propriétaire qui fume des Blue Master boit de la bière.
-appartient_à(maison(_,_,_,_,biere,blue_master),MAISONS),
+appartient_à(maison(_,_,_,biere,blue_master),MAISONS),
 
 % 13 - L'Allemand fume des Prince.
-appartient_à(maison(_,_,allemand,_,_,prince),MAISONS),
+appartient_à(maison(_,allemand,_,_,prince),MAISONS),
 
 % 14 - Le Norvégien vit juste à côté de la maison bleue.
-est_à_côté_de(maison(_,_,norvégien,_,_,_),maison(_,bleue,_,_,_,_),MAISONS),
+est_à_côté_de(maison(_,norvégien,_,_,_),maison(bleue,_,_,_,_),MAISONS),
 
 % 15 - L'homme qui fume des Blend a un voisin qui boit de l'eau.
-est_à_côté_de(maison(_,_,_,_,_,blend),maison(_,_,_,_,eau,_),MAISONS),
+est_à_côté_de(maison(_,_,_,_,blend),maison(_,_,_,eau,_),MAISONS),
 
 % Qui a le poisson ?
-appartient_à(maison(_,_,_,poisson,_,_),MAISONS),
+appartient_à(maison(_,_,poisson,_,_),MAISONS),
 
 % on affiche le résultat
 afficher_liste(MAISONS).
